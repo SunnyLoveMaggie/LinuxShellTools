d228479aab6369d164261b1064fe6aa375bcf94d
xiangmin2587 <xiangmin2587@163.com>
2014-06-23 10:50:18 +0800

    add a todo tools

--- sourceFile.sh
+++ sourceFile.sh
@@ -2,3 +2,8 @@ PWD="$HOME/Personal/timeSaving"
 source "$PWD/alias.sh"
 source "$PWD/z.sh"
 source "$PWD/mysh.sh"
+export PATH="$PATH:$HOME/Personal/timeSaving/todo"
+export TODOFILE=~/Personal/timeSaving/todo/todo-file
+export TODOHISTORY=~/Personal/timeSaving/todo/todo-history
+alias t=todo
+alias d=todone
