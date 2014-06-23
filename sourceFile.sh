4b210159e1ca3575cee655363b026313cb6ca440
xiangmin2587 <xiangmin2587@163.com>
2014-06-23 13:21:10 +0800

    add the tag for each todo

--- sourceFile.sh
+++ sourceFile.sh
@@ -6,4 +6,8 @@ export PATH="$PATH:$HOME/Personal/timeSaving/todo"
 export TODOFILE=~/Personal/timeSaving/todo/todo-file
 export TODOHISTORY=~/Personal/timeSaving/todo/todo-history
 alias t=todo
+alias t.l="todo [LOW]"
+alias t.m="todo [MEDIUM]"
+alias t.h="todo [HIGH]"
+
 alias d=todone
